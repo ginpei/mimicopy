931e4162e9abddf36b3185ab27fb886bc7b463e5
ginpei <ginpei@gmail.com>
2015-05-11 00:30:17 -0700

    accept files by dropping

--- mimicopy.js
+++ mimicopy.js
@@ -1,3 +1,44 @@
-document.querySelector('h1').addEventListener('click', function(event) {
-	alert('Hello!');
-});
+(function() {
+	var elBody = document.body;
+	var elDroppable = document.querySelector('html');
+	var elSoundList = document.querySelector('.js-soundList');
+
+	elDroppable.addEventListener('dragover', function(event) {
+		event.preventDefault();
+		elBody.classList.add('is-dragover');
+	});
+
+	elDroppable.addEventListener('dragleave', function(event) {
+		elBody.classList.remove('is-dragover');
+	});
+
+	elDroppable.addEventListener('drop', function(event) {
+		event.preventDefault();
+		elBody.classList.remove('is-dragover');
+
+		var files = event.dataTransfer.files;
+		var html = map(files, function(file, index) {
+			var html = '<li>';
+			html += escape(file.name);
+			html += '</li>';
+			return html;
+		}).join('');
+		console.log(files, html);
+		elSoundList.innerHTML += html;
+	});
+
+	function map(array, callback) {
+		return Array.prototype.map.call(array, callback);
+	}
+
+	function escape(string) {
+		var safe = string
+			.replace(/&/g, '&amp;')
+			.replace(/</g, '&lt;')
+			.replace(/>/g, '&gt;')
+			.replace(/"/g, '&quot;')
+			.replace(/'/g, '&quot;')
+			.replace(/`/g, '&quot;');
+		return safe;
+	};
+})();
