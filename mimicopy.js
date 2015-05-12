b70baf339682d7b1b3dfd682a9f755e8d2987463
ginpei <ginpei@gmail.com>
2015-05-12 03:06:08 -0700

    add libraries

--- mimicopy.js
+++ mimicopy.js
@@ -1,4 +1,34 @@
 (function() {
+	// gQuery Alpha
+	var $ = function(){function d(b){return this instanceof a?this.
+	_initialize(b):new a(b)}var a=d,b=a.fn=a.prototype,c=[].forEach;
+	return b._initialize=function(a){var b=this,d=b.nodeList=
+	document.querySelectorAll(a);return b.length=d.length,c.call(d,
+	function(a,c){b[c]=a}),b},b.forEach=function(a){return c.call(
+	this,a),this},a}();
+	$.extend = function(source, props) {
+		for (var name in props) { source[name] = props[name]; }
+	};
+
+	/*! Osteoporosis.js v1.0.1 By TAKANASHI Ginpei */
+	var O=function(){var t={},i="prototype",n="extend",e=
+	"trigger",o="attributes",r="_listeners",s=[].slice,u="undefined"
+	==typeof _?$[n]:_[n],a=function(){};t[n]=function(e,o){function
+	r(t){this.__osteoporosis__(t),this.initialize(t)}return r[n]=t[n
+	],u(r[i],this[i],e),u(r,o),r};var h=t.eventPrototype={on:
+	function(t,i){var n=this[r];n||(n=this[r]={});var e=n[t];e||(e=n
+	[t]=[]),e.push(i)},trigger:function(t){var i=this[r];if(i&&i[t])
+	{var n=s.call(arguments,1);i[t].forEach(function(t){t.apply(null
+	,n)})}}};return t.Model=function(){},t.Model[n]=t[n],u(t.Model[i
+	],{__osteoporosis__:function(t){return this[o]={},this.set(t)},
+	initialize:a,set:function(t){var i=this[o];for(var n in t){var r
+	=t[n],s=i[n];r!==s&&(i[n]=r,this[e]("change:"+n,this,r),this[e](
+	"change",this))}return this},get:function(t){return this[o][t]},
+	on:h.on,trigger:h[e]}),t.View=function(){},t.View[n]=t[n],u(t.
+	View[i],{__osteoporosis__:function(t){t=t||{},this.$el=$(t.el||
+	document)},initialize:a,$:function(t){return this.$el.find(t)},
+	on:h.on,trigger:h[e]}),t}();
+
 	var mimicopy = window.mimicopy = {
 		initialize: function() {
 			this.soundFileTable = { length:0 };
