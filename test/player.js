1804078af8e46f15707e4636ec3743af112557d9
ginpei <ginpei@gmail.com>
2015-05-14 04:53:26 -0700

    tidy up tests

--- test/player.js
+++ test/player.js
@@ -13,36 +13,51 @@
 			});
 		});
 
-		describe('duration', function() {
-			beforeEach(function() {
-				track.set({ duration:61.001 });
+		describe('time range', function() {
+			describe('duration', function() {
+				beforeEach(function() {
+					track.set({ duration:61.001 });
+				});
+
+				it('updates text', function() {
+					expect(player.$('.js-durationText').html()).toBe('1:01.001');
+				});
+
+				it('updates the maximum of time UIs', function() {
+					expect(player.$('.js-timeFrom')[0].max).toBe('61.001');
+					expect(player.$('.js-currentTime')[0].max).toBe('61.001');
+					expect(player.$('.js-timeTo')[0].max).toBe('61.001');
+				});
 			});
 
-			it('updates text', function() {
-				expect(player.$('.js-durationText').html()).toBe('1:01.001');
+			describe('timeFrom', function() {
+				beforeEach(function() {
+					track.set({ timeFrom:61.001 });
+				});
+
+				it('updates timeFrom UI', function() {
+					expect(player.$('.js-timeFrom')[0].value).toBe('61.001');
+				});
 			});
 
-			it('updates the maximum of time UIs', function() {
-				expect(player.$('.js-timeFrom')[0].max).toBe('61.001');
-				expect(player.$('.js-currentTime')[0].max).toBe('61.001');
-				expect(player.$('.js-timeTo')[0].max).toBe('61.001');
-			});
-		});
+			describe('currentTime', function() {
+				beforeEach(function() {
+					track.set({ currentTime:61.001 });
+				});
 
-		describe('UIs', function() {
-			it('updates timeFrom UI', function() {
-				track.set({ timeFrom:61.001 });
-				expect(player.$('.js-timeFrom')[0].value).toBe('61.001');
+				it('updates currentTime UI', function() {
+					expect(player.$('.js-currentTime')[0].value).toBe('61.001');
+				});
 			});
 
-			it('updates currentTime UI', function() {
-				track.set({ currentTime:61.001 });
-				expect(player.$('.js-currentTime')[0].value).toBe('61.001');
-			});
+			describe('timeTo', function() {
+				beforeEach(function() {
+					track.set({ timeTo:61.001 });
+				});
 
-			it('updates timeTo UI', function() {
-				track.set({ timeTo:61.001 });
-				expect(player.$('.js-timeTo')[0].value).toBe('61.001');
+				it('updates timeTo UI', function() {
+					expect(player.$('.js-timeTo')[0].value).toBe('61.001');
+				});
 			});
 		});
 	});
