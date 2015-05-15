ce6670343f5793421739244b07e949295e6f0bea
ginpei <ginpei@gmail.com>
2015-05-14 21:45:50 -0700

    order

--- scripts/mimicopy.js
+++ scripts/mimicopy.js
@@ -27,19 +27,21 @@
 			this._setupTimeTo(track);
 
 			this.$durationText = this.$('.js-durationText');
-			this.$timeFromText = this.$('.js-timeFromText');
-			this.$currentTimeText = this.$('.js-currentTimeText');
-			this.$timeToText = this.$('.js-timeToText');
-
 			track.on('change:duration', function(track, value) {
 				this.$durationText.html(this.timeText(value));
 			}.bind(this));
+
+			this.$timeFromText = this.$('.js-timeFromText');
 			track.on('change:timeFrom', function(track, value) {
 				this.$timeFromText.html(this.timeText(value));
 			}.bind(this));
+
+			this.$currentTimeText = this.$('.js-currentTimeText');
 			track.on('change:currentTime', function(track, value) {
 				this.$currentTimeText.html(this.timeText(value));
 			}.bind(this));
+
+			this.$timeToText = this.$('.js-timeToText');
 			track.on('change:timeTo', function(track, value) {
 				this.$timeToText.html(this.timeText(value));
 			}.bind(this));
