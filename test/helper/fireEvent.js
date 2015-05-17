e2d6e9b3408f5de273153203b72108a77f71167a
ginpei <ginpei@gmail.com>
2015-05-16 22:27:20 -0700

    extend event object for tests

--- test/helper/fireEvent.js
+++ test/helper/fireEvent.js
@@ -1,5 +1,6 @@
-test_helper_fireEvent = function(el, type) {
+test_helper_fireEvent = function(el, type, extention) {
 	var event = document.createEvent('Events');  // instantiating via CustomEvent constructor is not supported in IE 11
 	event.initEvent(type, true, true);
+	gQuery.extend(event, extention);
 	el.dispatchEvent(event);
 };
